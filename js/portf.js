f8e378c48863b3e3dc8cfa7c973177e7b92b0a8c
CaioRibeiro94 <54272601+CaioRibeiro94@users.noreply.github.com>
2023-11-29 23:12:50 -0300

    Add files via upload

---
+++ js/portf.js
@@ -0,0 +1,48 @@
+// MENU NAVBAR
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+
+
+// MENU ACTIVE
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+                navLinks.forEach(links => {
+                    links.classList.remove('active');
+                    document.querySelector('header nav a[href*='+ id + ']').classList.add('active');
+                })
+        }
+    })
+}
+
+//    HEADER ROLAGEM  //
+
+window.onscroll = () =>{
+
+    let header = document.querySelector("header")
+
+    header.classList.toggle('rolagem', window.scrollY > 100)
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+}
+
+// btnMobile.addEventListener('click', toggleMenu);
+// btnMobile.addEventListener('touchstart', toggleMenu);
+
+/////////////
